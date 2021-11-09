7b536ae9092ee41d888a46964f5f9b79eb576142
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2021-11-09 12:57:57 +0900

    새로운 checkwinner 알고리즘 실험
     *  ---업데이트 내용---
     *  새로운 checkwinner 알고리즘 실험
     *  winner를 마지막에 디스플레이

--- tictactoe_simple.py
+++ tictactoe_simple.py
@@ -2,6 +2,10 @@
  *  tictactoe_simple.cpp
  * Created on: 2021. 10. 27.
  *     Author: CSY, KLY
+	
+ *  ---업데이트 내용---
+ *  새로운 checkwinner 알고리즘 실험
+ *  winner를 마지막에 디스플레이  
  */
 
 #include <stdio.h>
@@ -27,6 +31,12 @@ enum player{
 	PLAYER_B =1
 };
 
+class ReplayInfo{
+  public:
+    char PlayerName;
+    unsigned char wherePosition;
+};
+
 class Position{
 public:
 	int y;
@@ -36,18 +46,25 @@ public:
 
 class tictactoe{
 private:
+  unsigned char GameCount;
 	int x; //player1 first
 	int o; //player2
 	Position cursorPos;
 	Position LUT_1dto2d[9] = { {0,0}, {0,1}, {0,2},
 					                	{1,0}, {1,1}, {1,2},
 				                		{2,0}, {2,1}, {2,2} };
-	player currentPlayer = PLAYER_A;
+                            
+  player nextPlayer = PLAYER_A;
+	player currentPlayer;
+  player winnerPlayer;
 	bool gameover = false;
 
 	char gameboard[3][3] = {{ '1', '2', '3' },
 							{ '4', '5', '6' },
 							{ '7', '8', '9' } };
+
+  
+  
 	//char field[3][3];
 	// field[9] = {1,2,3,4,5,6,7,8,9}
   
@@ -62,7 +79,13 @@ public:
 	void inputPos(void);
 	int checkGameover(void);
   bool GetGameover(void);
+  player getWinner(void);
+  ReplayInfo PlayHistory[9];
 };
+
+player tictactoe::getWinner(void){
+  return winnerPlayer;
+}
 
 bool  tictactoe::checkLine(char cell1, char cell2, char cell3){
   if(cell1== cell2 && cell2 == cell3){
@@ -73,25 +96,49 @@ bool  tictactoe::checkLine(char cell1, char cell2, char cell3){
 }
 
 int tictactoe::checkGameover(void){
-    for(int i=0; i<3; i++){
+    char firstValue;
+    for(int j=0; j<3; j++){
+      for(int i=0; i<3; i++){
+        if (gameboard[i][j] == 'o' || gameboard[i][j] == 'x'){
+          if (i==0){
+            firstValue = gameboard[i][j];
+          }else{
+            if(firstValue != gameboard[i][j]){
+              break;
+            }
+            else if ((firstValue == gameboard[i][j]) && (i==2)){
+              SetGameover(true);
+              winnerPlayer = currentPlayer;
+            }
+          }
+        }else{
+          break;
+        }
+      }
+    }
+
+    /*for(int i=0; i<3; i++){
       if(checkLine(gameboard[i][0],gameboard[i][1],gameboard[i][2])){
         SetGameover(true);
         break;
       }
-    }
+    }*/
     for(int j=0; j<3; j++){
       if(checkLine(gameboard[0][j],gameboard[1][j],gameboard[2][j])){
         SetGameover(true);
+        winnerPlayer = currentPlayer;
         break;
       }
     }
     if(checkLine(gameboard[0][0],gameboard[1][1] ,gameboard[2][2])){
       SetGameover(true);
+      winnerPlayer = currentPlayer;
       return 0;
     }
 
     if(checkLine(gameboard[0][2],gameboard[1][1] ,gameboard[2][0])){
       SetGameover(true);
+      winnerPlayer = currentPlayer;
       return 0;
     }
 
@@ -158,16 +205,24 @@ void tictactoe::inputPos(void){
 	//cursorPos.y=LUT_1dto2d[pos-1].y;
   getCursor();
 
+  pos = (cursorPos.y+1)*3 + (cursorPos.x+1);
 
   if((gameboard[cursorPos.y][cursorPos.x]!='o')&&
      (gameboard[cursorPos.y][cursorPos.x]!='x')){
+
+    currentPlayer = nextPlayer;
+    PlayHistory[GameCount].PlayerName = currentPlayer;
+    PlayHistory[GameCount].wherePosition = pos;
+    
+    GameCount++;
     if(currentPlayer == PLAYER_A){
       gameboard[cursorPos.y][cursorPos.x]='o';
-      currentPlayer = PLAYER_B;
-    }else {
+      nextPlayer = PLAYER_B;
+    }else { //playerB
       gameboard[cursorPos.y][cursorPos.x]='x';
-      currentPlayer = PLAYER_A;
+      nextPlayer = PLAYER_A;
     }
+
   }
 }
 
@@ -175,21 +230,34 @@ void tictactoe::inputPos(void){
 tictactoe::tictactoe(void){
 	cursorPos.x = 1;
 	cursorPos.y = 1;
+  GameCount =0;
 }
+
 
 
 // T E M P
 int main(void){
 	char gb;
 	int i;
+  string playerID[2] ={"789o", "x"};
+ 
+
   //clear();
   //gotoxy(1,1);
 	tictactoe game;
+
 	while(game.GetGameover()==false){
 		game.drawBoard();
 		game.inputPos();
     game.checkGameover();
 	}
+  game.drawBoard();
+
+  // y = 'A' or 'B' , x=winnerPlayer {PLAYER_A or PLAYER_B}  
+  cout<< playerID[game.getWinner()] << " is winner" <<endl;
+
+
+  
 }
 
 int getch(void) {
